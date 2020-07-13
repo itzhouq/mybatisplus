1e0fa9513040a04b864d111d6fcc60c4417d0c87
itzhouq <itzhouq@163.com>
2020-07-14 00:12:11 +0800

    upload SQL file.

---
+++ src/db/mybatisplus.sql
@@ -0,0 +1,43 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50729
+ Source Host           : localhost:3306
+ Source Schema         : mybatisplus
+
+ Target Server Type    : MySQL
+ Target Server Version : 50729
+ File Encoding         : 65001
+
+ Date: 14/07/2020 00:10:24
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL COMMENT '主键ID',
+  `name` varchar(30) DEFAULT NULL COMMENT '姓名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (1, 'Jone', 18, 'test1@baomidou.com');
+INSERT INTO `user` VALUES (2, 'Jack', 20, 'test2@baomidou.com');
+INSERT INTO `user` VALUES (3, 'Tom', 28, 'test3@baomidou.com');
+INSERT INTO `user` VALUES (4, 'Sandy', 21, 'test4@baomidou.com');
+INSERT INTO `user` VALUES (5, 'Billie', 24, 'test5@baomidou.com');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
